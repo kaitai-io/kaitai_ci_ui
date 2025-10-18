80e8e5ac4710c7af3af59fce955aac46bc012ebc
Petr Pucil <petr.pucil@seznam.cz>
2025-10-18 04:10:12 +0200

    Add Zig 0.15.2
    See https://github.com/kaitai-io/ci_targets/commit/70cf54d6465f3b18a495c16f6725803f5ba9b981

--- src/targetPairs.js
+++ src/targetPairs.js
@@ -42,6 +42,7 @@ const TARGET_PAIRS = [
   ["ruby", "2.3-linux-x86_64"],
   ["ruby", "3.4-linux-x86_64"],
   ["rust", "1.89-linux-x86_64"],
+  ["zig", "0.15.2-linux-x86_64"],
 ];
 TARGET_PAIRS.forEach(pair => Object.freeze(pair));
 Object.freeze(TARGET_PAIRS);
